2bb45ccae963cdd1a63a3eb2bb0c34108292dfa2
Parker Hutchinson <parker.hutch@outlook.com>
2024-05-08 21:48:34 -0400

    add vector add example

--- Main.lean
+++ Main.lean
@@ -5,16 +5,16 @@ import Mathlib.Algebra.AddTorsor
 import Mathlib.Data.Vector        --https://github.com/leanprover-community/mathlib4/blob/a7ed535af2a1f78accefeaeee98233dd25714110/Mathlib/Data/Vector.lean#L20-L21
 
 -- Define some ℚ n-tuples
-def t1 : Vector ℚ 3 := ⟨ [0, (1/2:ℚ), 2], rfl ⟩ 
-def t2 : Vector ℚ 3 := ⟨ [2, (-1/2:ℚ), -2], rfl ⟩ 
+def t1 : Vector ℚ 3 := ⟨ [0, (1/2:ℚ), 2], rfl ⟩
+def t2 : Vector ℚ 3 := ⟨ [2, (-1/2:ℚ), -2], rfl ⟩
 
--- see tuple toString delegate to List toString 
+-- see tuple toString delegate to List toString
 #eval s!"{t1}"
 
 
 -- Define some rational affine 3-points
-def p1 : AffPoint ℚ 3  := ⟨ t1 ⟩ 
-def p2 : AffPoint ℚ 3  := ⟨ t2 ⟩ 
+def p1 : AffPoint ℚ 3  := ⟨ t1 ⟩
+def p2 : AffPoint ℚ 3  := ⟨ t2 ⟩
 
 -- AffPt toString
 #eval s!"{p1}"
@@ -22,6 +22,9 @@ def v := p2 -ᵥ p1
 
 #eval s!"{v}"
 
+def v2 := p2 +ᵥ p1
+
+#eval s!"{v2}"
 
 #check ToString (AffPoint ℚ 3)
 
@@ -32,6 +35,7 @@ def main : IO Unit :=
     IO.println s!"p1 = {p1}"
     IO.println s!"p2 = {p2}"
     IO.println s!"v = p1 -ᵥ p2 = {v}"
+    IO.println s!"v2 = p1 +ᵥ p2 = {v2}"
 
 
 #eval main
